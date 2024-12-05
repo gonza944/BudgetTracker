70ed47aaf490d7fd70d44f92a7a13058a768f7d2
Gonzalo Ariza Sabbatini <gonza94.4@gmail.com>
2024-12-05 15:23:11 -0300

    connect with the Database (#3)

--- src/app/dashboard/components/overallBalance.tsx
+++ src/app/dashboard/components/overallBalance.tsx
@@ -1,8 +1,13 @@
-const OverallBalance: React.FC = () => {
+import { Redis } from "@upstash/redis";
+
+const OverallBalance: React.FC = async() => {
+  const redis = Redis.fromEnv();
+  const count = await redis.incr("counter");
+
   return (
     <>
       <h1 className="font-title text-5xl">Overall Balance</h1>
-      <h2 className="font-title text-7xl text-primaryColor m-4">$ 500</h2>
+      <h2 className="font-title text-7xl text-primaryColor m-4">$ {count}</h2>
     </>
   );
 };
