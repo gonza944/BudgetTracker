eff1f302f5193f8b5741ecf9a9b28f3f995dc8a0
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2024-12-06 21:15:54 +0200

    feat: create new project

---
+++ src/app/@new-budget-modal/(.)new-budget/page.tsx
@@ -0,0 +1,9 @@
+import { Modal } from "@/utils/modal";
+
+const NewBudgetModal: React.FC = () => (
+  <Modal>
+      <h1>some New Budget</h1>
+  </Modal>
+);
+
+export default NewBudgetModal;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Balance from "./components/balance";
 import ExpensesList from "./components/expensesList";
 import OverallBalance from "./components/overallBalance";
@@ -5,6 +6,7 @@ import OverallBalance from "./components/overallBalance";
 const Dashboard: React.FC = () => {
   return (
     <div className=" grid flex-col grid-cols-6 gap-6 max-md:grid-cols-1">
+
       <div className="flex flex-col items-start max-md:items-center col-start-3 col-span-3 max-md:col-start-1 max-md:col-span-1 row-span-1 flex-wrap">
         <OverallBalance />
       </div>
