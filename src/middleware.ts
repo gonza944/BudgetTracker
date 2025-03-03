0cba21387cf1e6e10fd5babf6c69db7ae7abf8bb
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2025-03-03 18:26:51 -0300

    Fix: forward fix log in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { auth } from "@/auth"
  
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
+  /* if (!req.auth && req.nextUrl.pathname !== "/log-in") {
     const newUrl = new URL("/log-in", req.nextUrl.origin)
     return Response.redirect(newUrl)
-  }
+  } */
 })
