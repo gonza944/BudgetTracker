419cb8181bf485137b25f6a6b503896f376c7db6
Gonzalo Ariza <gonza94.4@gmail.com>
2025-04-07 08:09:07 -0300

    add shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,83 @@
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        title: ["var(--font-freudian)", "serif"],
-        paragraph: ["eczar"],
-      },
-      colors: {
-        backgroundColor: "var(--green)",
-        neutralBackgroundColor: "var(--gray2)",
-        neutralBackgroundColorInverted: "var(--gray1)",
-        primaryColor: "var(--green)",
-        accentColor: "var(--yellow)",
-        secondaryAccentColor: "var(--darkRed)",
-        fontColor: "var(--gray1)",
-        fontColorInverted: "var(--gray2)",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        grain: "url('../../public/noise2.png')",
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			title: [
+  				'var(--font-freudian)',
+  				'serif'
+  			],
+  			paragraph: [
+  				'eczar'
+  			]
+  		},
+  		colors: {
+  			backgroundColor: 'var(--green)',
+  			neutralBackgroundColor: 'var(--gray2)',
+  			neutralBackgroundColorInverted: 'var(--gray1)',
+  			primaryColor: 'var(--green)',
+  			accentColor: 'var(--yellow)',
+  			secondaryAccentColor: 'var(--darkRed)',
+  			fontColor: 'var(--gray1)',
+  			fontColorInverted: 'var(--gray2)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  			grain: 'url('../../public/noise2.png')'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
